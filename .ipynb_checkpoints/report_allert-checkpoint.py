bfa7fc99d644d6baa69ee21b2be873665e285360
NikitaIvanov <Espada007@mail.ru>
2022-03-30 15:26:48 +0000

    report allert code is donde

--- .ipynb_checkpoints/report_allert-checkpoint.py
+++ .ipynb_checkpoints/report_allert-checkpoint.py
@@ -16,30 +16,27 @@ connection = {
     'database': 'simulator_20220220'
 }
 
-def check_anomaly(df, metric, threshold=0.3):
+def check_anomaly(df_metric, metric, a=4, n=5):
     # функция check_anomaly предлагает алгоритм проверки значения на аномальность посредством
-    # сравнения интересующего значения со значением в это же время сутки назад
-    # при желании алгоритм внутри этой функции можно изменить
-    current_ts = df['ts'].max()  # достаем максимальную 15-минутку из датафрейма - ту, которую будем проверять на аномальность
-    day_ago_ts = current_ts - pd.DateOffset(days=1)  # достаем такую же 15-минутку сутки назад
-
-    current_value = df[df['ts'] == current_ts][metric].iloc[0] # достаем из датафрейма значение метрики в максимальную 15-минутку
-    day_ago_value = df[df['ts'] == day_ago_ts][metric].iloc[0] # достаем из датафрейма значение метрики в такую же 15-минутку сутки назад
-
-    # вычисляем отклонение
-    if current_value <= day_ago_value:
-        diff = abs(current_value / day_ago_value - 1)
-    else:
-        diff = abs(day_ago_value / current_value - 1)
-
+    # межквартильного размаха  IQR
+    #shift(1) - сдвигаем массив на 1 вперед, в итоге рассчет ведется не включая последнего значения (оно мб дефектным)
+    df_metric['q25'] = df_metric.metric.shift(1).rolling(n).quantile(0.25)
+    df_metric['q75'] = df_metric.metric.shift(1).rolling(n).quantile(0.75)
+    df_metric['IQR'] =  df_metric['q75'] - df_metric['q25']
+    df_metric['up_border'] = df_metric['q25']-a*df_metric['IQR']
+    df_metric['low_border'] = df_metric['q75']+a*df_metric['IQR']
+    
+    df_metric['up_border'] = df_metric['up_border'].rolling(n, center = True, min_periods = 1)
+    df_metric['low_border'] = df_metric['low_border'].rolling(n, center = True, min_periods = 1)
+    
     # проверяем больше ли отклонение метрики заданного порога threshold
     # если отклонение больше, то вернем 1, в противном случае 0
-    if diff > threshold:
+    if df_metric.metric.iloc[-1] > df_metric['up_border'].iloc[-1] or df_metric.metric.iloc[-1] < df_metric['low_border'].iloc[-1]
         is_alert = 1
     else:
         is_alert = 0
 
-    return is_alert, current_value, diff
+    return is_alert, df_metric
 
 
 def run_alerts(chat=None):
@@ -109,47 +106,45 @@ def run_alerts(chat=None):
                     group by Full_time, date, Hour_min
                     order by Full_time '''
     df_allert = pandahouse.read_clickhouse(q, connection=connection)
+    df_allert['CTR'] = round(df_allert.likes*100/df_allert.views,2)
     
-    
-    
-    
-    metric = 'users_lenta'
-    is_alert, current_value, diff = check_anomaly(data, metric, threshold=0.1) # проверяем метрику на аномальность алгоритмом, описаным внутри функции check_anomaly()
-    if is_alert:
-        msg = '''Метрика {metric}:\nтекущее значение = {current_value:.2f}\nотклонение от вчера {diff:.2%}'''.format(metric=metric,
-                                                                                                                     current_value=current_value,
-                                                                                                                     diff=diff)
+    for metric in df_allert.iloc[:, 3:].columns:
+        df_metric =   df_allert[['Full_time', 'date', 'Hour_min', metric]].copy()   
+        is_alert, df_metric = check_anomaly(df_metric, metric) # проверяем метрику на аномальность алгоритмом, описаным внутри функции check_anomaly()
+        
+        if is_alert = 1:
+            msg = '''Метрика {metric}:\nтекущее значение = {current_value:.2f}\nотклонение от вчера {diff:.2%}'''\
+                    .format(metric=metric, current_value=df_metric.metric.iloc[-1], diff = df_metric.metric.iloc[-2])
+                                                                                                                         
 
-        sns.set(rc={'figure.figsize': (16, 10)}) # задаем размер графика
-        plt.tight_layout()
+            sns.set(rc={'figure.figsize': (16, 10)}) # задаем размер графика
+            plt.tight_layout()
 
-        ax = sns.lineplot( # строим линейный график
-            data=data.sort_values(by=['date', 'hm']), # задаем датафрейм для графика
-            x="hm", y=metric, # указываем названия колонок в датафрейме для x и y
-            hue="date" # задаем "группировку" на графике, т е хотим чтобы для каждого значения date была своя линия построена
-            )
+            ax = sns.lineplot(data = df_metric, x = 'Full_time', y = metric, label = 'metric')
+            ax = sns.lineplot(data = df_metric, x = 'Full_time', y = 'up_border', label = 'up_border')
+            ax = sns.lineplot(data = df_metric, x = 'Full_time', y = 'low_border', label = 'low_border')
 
-        for ind, label in enumerate(ax.get_xticklabels()): # этот цикл нужен чтобы разрядить подписи координат по оси Х,
-            if ind % 15 == 0:
-                label.set_visible(True)
-            else:
-                label.set_visible(False)
-        ax.set(xlabel='time') # задаем имя оси Х
-        ax.set(ylabel=metric) # задаем имя оси У
+            for ind, label in enumerate(ax.get_xticklabels()): # этот цикл нужен чтобы разрядить подписи координат по оси Х,
+                if ind % 2 == 0:
+                    label.set_visible(True)
+                else:
+                    label.set_visible(False)
+            ax.set(xlabel='time') # задаем имя оси Х
+            ax.set(ylabel=metric) # задаем имя оси У
 
-        ax.set_title('{}'.format(metric)) # задае заголовок графика
-        ax.set(ylim=(0, None)) # задаем лимит для оси У
+            ax.set_title('{}'.format(metric)) # задае заголовок графика
+            ax.set(ylim=(0, None)) # задаем лимит для оси У
 
-        # формируем файловый объект
-        plot_object = io.BytesIO()
-        ax.figure.savefig(plot_object)
-        plot_object.seek(0)
-        plot_object.name = '{0}.png'.format(metric)
-        plt.close()
+            # формируем файловый объект
+            plot_object = io.BytesIO()
+            ax.figure.savefig(plot_object)
+            plot_object.seek(0)
+            plot_object.name = '{0}.png'.format(metric)
+            plt.close()
 
-        # отправляем алерт
-        bot.sendMessage(chat_id=chat_id, text=msg)
-        bot.sendPhoto(chat_id=chat_id, photo=plot_object)
+            # отправляем алерт
+            bot.sendMessage(chat_id=chat_id, text=msg)
+            bot.sendPhoto(chat_id=chat_id, photo=plot_object)
 
 
 try:
